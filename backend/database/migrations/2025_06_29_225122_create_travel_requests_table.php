97fb29d362e27456e6467fbf8964c625557bb558
Cesar <csarfau@gmail.com>
2025-06-29 20:07:09 -0300

    feat: create travel_resquests table and relationship with user

---
+++ backend/database/migrations/2025_06_29_225122_create_travel_requests_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('travel_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('requester_name');
+            $table->string('destination');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->string('status')->default('requested');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_requests');
+    }
+};
