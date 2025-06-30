84f7b2495eae822e5d9a363d0092b68322bd230d
Cesar <csarfau@gmail.com>
2025-06-30 15:43:01 -0300

    test: add comprehensive tests to create a new travel request

---
+++ backend/tests/Feature/TravelRequestTest.php
@@ -0,0 +1,112 @@
+<?php
+
+use App\Models\User;
+use Carbon\Carbon;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+
+it('an authenticated user must be able create a travel request', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+    $destination = fake()->city();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'), [
+        'requester_name'    => $user->name,
+        'destination'       => $destination,
+        'departure_date'    => today()->format('d/m/Y'),
+        'return_date'       => today()->addDays(10)->format('d/m/Y'),
+    ])->assertCreated();
+
+    assertDatabaseHas('travel_requests', [
+        'user_id' => $user->id,
+        'destination' => $destination
+    ]);
+});
+
+it('requester name is required', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'))
+        ->assertJsonValidationErrors([
+            'requester_name' => __('validation.required', ['attribute' => 'requester name'])
+        ]);
+});
+
+it('requester name must be max of 255 characters', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'), [
+        'requester_name' => str_repeat('a', 256)
+    ])->assertJsonValidationErrors([
+        'requester_name' => __('validation.max.string', ['attribute' => 'requester name', 'max' => 255])
+    ]);
+});
+
+it('destination is required', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'))
+        ->assertJsonValidationErrors([
+            'destination' => __('validation.required', ['attribute' => 'destination'])
+        ]);
+});
+
+it('destination must be max of 255 characters', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'), [
+        'destination' => str_repeat('a', 256)
+    ])->assertJsonValidationErrors([
+        'destination' => __('validation.max.string', ['attribute' => 'destination', 'max' => 255])
+    ]);
+});
+
+it('departure date is required', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'))
+        ->assertJsonValidationErrors([
+            'departure_date' => __('validation.required', ['attribute' => 'departure date'])
+        ]);
+});
+
+it('return date is required', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'))
+        ->assertJsonValidationErrors([
+            'return_date' => __('validation.required', ['attribute' => 'return date'])
+        ]);
+});
+
+it('departure date should be a valid date (after or equal today)', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'), [
+        'departure_date' => Carbon::yesterday()->format('d-m-Y')
+    ])
+        ->assertJsonValidationErrors([
+            'departure_date' => __('validation.after_or_equal', ['attribute' => 'departure date', 'date' => 'today'])
+        ]);
+});
+
+it('return date should be a valid date (after or equal departure date)', function () {
+    /** @var User $user */
+    $user = User::factory()->createOne();
+
+    actingAs($user, 'api')->postJson(route('travel-request.store'), [
+        'departure_date' => today()->addDays(5)->format('d-m-Y'),
+        'return_date' => today()->format('d-m-Y')
+    ])
+        ->assertJsonValidationErrors([
+            'return_date' => __('validation.after_or_equal', ['attribute' => 'return date', 'date' => 'departure date'])
+        ]);
+});
