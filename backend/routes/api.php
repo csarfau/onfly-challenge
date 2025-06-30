64524c23de767299d17eff756a65d735b4bf8540
Cesar <csarfau@gmail.com>
2025-06-30 15:40:44 -0300

    Revert "feat: add travel request create route"
    This reverts commit 2a4f9782741a895cba9a0225562a033bf6e4c137.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TravelRequestController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
@@ -12,5 +11,4 @@ Route::group(['prefix' => 'auth'], function () {
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
-    Route::post('/travel-request/create', [TravelRequestController::class, 'store'])->name('travel-request.store');
 });
