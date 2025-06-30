108250ad8a4b1d6d710cc4c69f4640a2eb9390d5
Cesar <csarfau@gmail.com>
2025-06-30 10:37:33 -0300

    feat: add login route and group routes with api middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register'])
-    ->name('register');
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
