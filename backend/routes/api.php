7911bc75fff06c22122dcc7458e11791331bc58d
Cesar <csarfau@gmail.com>
2025-06-30 15:32:59 -0300

    feat: add travel request create route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelRequestController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
@@ -11,4 +12,5 @@ Route::group(['prefix' => 'auth'], function () {
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/travel-request/create', [TravelRequestController::class, 'store'])->name('travel-request.store');
 });
