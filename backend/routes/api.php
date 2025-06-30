af8ee1432382ca5e95b4b53572b8a98b588a83ed
Cesar <csarfau@gmail.com>
2025-06-30 11:14:07 -0300

    feat: add logout route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
 });
