bc31be51591239ae3afbf626a1538251f84a81c7
Cesar <csarfau@gmail.com>
2025-07-01 21:51:31 -0300

    style: remove unecessary lines

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'onfly_challenge_db:6162/api',
     baseURL: 'http://127.0.0.1:6162/api',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
 axiosInstance.interceptors.request.use((config) => {
     config.headers.Authorization = axiosInstance.defaults.headers.common.Authorization
         ? axiosInstance.defaults.headers.common.Authorization
@@ -14,4 +14,5 @@ axiosInstance.interceptors.request.use((config) => {
     axiosInstance.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config
 })
+
 export default axiosInstance;
